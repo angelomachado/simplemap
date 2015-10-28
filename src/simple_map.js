4c1661e9a3b564d335ee5fd0602470360de4d81d
Kenner Kliemann <kenner.hp@gmail.com>
2015-10-28 09:46:45 -0200

    Apply jshint suggestions and fix small things

--- src/simple_map.js
+++ src/simple_map.js
@@ -1,20 +1,15 @@
-/*
-  Angelo D. Machado - 2015
-  https://github.com/geloh
-  The MIT License (MIT)
-  Copyright (c) 2015 Angelo Machado
-*/
-
 (function($) {
   'use strict';
+
   //Some default options
   var defaults = {
-    maxMarkers : "unlimited", //markers max number permitted
+    maxMarkers : 'unlimited', //markers max number permitted
     blockMarkers : false, //do we block adding markers?
     infoWindow : false, //do we use infowindow?
     zoom : 12, //initial zoom
     afterAddMarker : false //execute some code after add marker
   };
+
   //allow public access
   var public_methods = {
     addMarkers : addMarkers,
@@ -22,8 +17,10 @@
     showMarkers : showMarkers,
     deleteMarkers : deleteMarkers
   };
+
   //Array of map instances
   var maps = [];
+
   $.fn.SimpleMap = function() {
     //Calling the public methods
     if (typeof arguments[0] === 'string') {
@@ -52,10 +49,10 @@
     return this;
   };
   //Private methods
-  function initialize(args){
+  function initialize(){
     //Locating the coords
     var s = this.selector;
-    if (maps[s].options.lat!=undefined && maps[s].options.lng!=undefined){
+    if (maps[s].options.lat !== undefined && maps[s].options.lng !== undefined){
       var location = new google.maps.LatLng(maps[s].options.lat, maps[s].options.lng);
       maps[s].map.setCenter(location);
     }else {
@@ -65,8 +62,11 @@
   }
   //l = location; s = selector;
   function addMarker(l,s) {
-    if (((maps[s].options.maxMarkers > maps[s].markers.length)||(maps[s].options.maxMarkers=="unlimited"))
-        &&!maps[s].options.blockMarkers){
+    if ((
+        (maps[s].options.maxMarkers > maps[s].markers.length) ||
+        (maps[s].options.maxMarkers === 'unlimited')) &&
+        (!maps[s].options.blockMarkers)){
+
       var marker = new google.maps.Marker({
         position: l,
         map: maps[s].map
@@ -75,22 +75,27 @@
       //Open info window?
       if (maps[s].options.infoWindow){
         var iwc = {};//infoWindowContent
-        if (maps[s].options.infoWindow.title!=undefined)
+
+        if (maps[s].options.infoWindow.title !== undefined) {
           iwc.title = maps[selector].options.infoWindow.title;
-        if (maps[s].options.infoWindow.content!=undefined)
+        }
+
+        if (maps[s].options.infoWindow.content !== undefined){
           iwc.content=maps[s].options.infoWindow.content;
+        }
+
         var infowindow = new google.maps.InfoWindow(iwc);
-        //binds the marker click
-        google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', function() { //binds the marker click
           infowindow.open(maps[s].map,marker);
         });
       }
       //call the afterAddmarker function if passed
-      if (typeof maps[s].options.afterAddMarker=="function"){
+      if (typeof maps[s].options.afterAddMarker === 'function'){
         maps[s].options.afterAddMarker(marker);
       }
     }
-  };
+  }
+
   //adding markers via javascript
   function addMarkers(_markers) {
     var s = this.selector;
@@ -103,10 +108,14 @@
       //Open info window?
       if (marker.infoWindow){
         var infoWindowContent = {};
-        if (marker.infoWindow.title!=undefined)
+        if (marker.infoWindow.title !== undefined){
           infoWindowContent.title = marker.infoWindow.title;
-        if (marker.infoWindow.content!=undefined)
+        }
+
+        if (marker.infoWindow.content !== undefined){
           infoWindowContent.content=marker.infoWindow.content;
+        }
+
         var infowindow = new google.maps.InfoWindow(infoWindowContent);
         //binds the marker click
         google.maps.event.addListener(m, 'click', function() {
@@ -115,9 +124,9 @@
       }
     });
     setMarkers(s,maps[s].map);
-  };
-  //where are u?
-  function geoLocate(){
+  }
+
+  function geoLocate(){ //where are u?
     var selector = this.selector;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -129,32 +138,37 @@
     }else{
       noGeoLocationSuported();
     }
-  };
+  }
+
   function noGeoLocationSuported() {
-    console.log("No geolocation supported!");
-    console.log("Update your browser and ensure a better experience!");
-  };
+    console.log('No geolocation supported!');
+    console.log('Update your browser and ensure a better experience!');
+  }
+
   // Sets the map on all markers in the array.
   //s = selector; v=value(map instance or null)
   function setMarkers(s,v) {
     for (var i = 0; i < maps[s].markers.length; i++) {
       maps[s].markers[i].setMap(v);
     }
-  };
+  }
+
   // Removes the markers from the map, but keeps them in the array.
   function hideMarkers() {
     var s = this.selector;
     setMarkers(s,null);
-  };
+  }
+
   // Shows any markers currently in the array.
   function showMarkers() {
     var s = this.selector;
     setMarkers(s,maps[s].map);
-  };
+  }
+
   // Deletes all markers in the array by removing references to them.
   function deleteMarkers() {
     var s = this.selector;
     setMarkers(s,null);
     maps[s].markers = [];
-  };
+  }
 })(jQuery);
